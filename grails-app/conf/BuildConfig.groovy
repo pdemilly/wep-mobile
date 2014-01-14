e1841665f2c3554a0220e6523fe81875b8e798e6
Pascal DeMilly <pascal@dev.newgenesys.com>
2014-01-14 17:18:07 -0500

    added ionic framework

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -1,16 +1,27 @@
-grails.servlet.version = "2.5" // Change depending on target container compliance (2.5 or 3.0)
+grails.servlet.version = "3.0" // Change depending on target container compliance (2.5 or 3.0)
 grails.project.class.dir = "target/classes"
 grails.project.test.class.dir = "target/test-classes"
 grails.project.test.reports.dir = "target/test-reports"
+grails.project.work.dir = "target/work"
 grails.project.target.level = 1.6
 grails.project.source.level = 1.6
 //grails.project.war.file = "target/${appName}-${appVersion}.war"
 
-// uncomment (and adjust settings) to fork the JVM to isolate classpaths
-//grails.project.fork = [
-//   run: [maxMemory:1024, minMemory:64, debug:false, maxPerm:256]
-//]
+grails.project.fork = [
+    // configure settings for compilation JVM, note that if you alter the Groovy version forked compilation is required
+    //  compile: [maxMemory: 256, minMemory: 64, debug: false, maxPerm: 256, daemon:true],
 
+    // configure settings for the test-app JVM, uses the daemon by default
+    test: [maxMemory: 768, minMemory: 64, debug: false, maxPerm: 256, daemon:true],
+    // configure settings for the run-app JVM
+    run: [maxMemory: 768, minMemory: 64, debug: false, maxPerm: 256, forkReserve:false],
+    // configure settings for the run-war JVM
+    war: [maxMemory: 768, minMemory: 64, debug: false, maxPerm: 256, forkReserve:false],
+    // configure settings for the Console UI JVM
+    console: [maxMemory: 768, minMemory: 64, debug: false, maxPerm: 256]
+]
+
+grails.project.dependency.resolver = "ivy" // or ivy
 grails.project.dependency.resolution = {
     // inherit Grails' default dependencies
     inherits("global") {
@@ -24,49 +35,45 @@ grails.project.dependency.resolution = {
     repositories {
         inherits true // Whether to inherit repository definitions from plugins
 
-        grailsPlugins()
-        grailsHome()
-        grailsCentral()
+        // grailsPlugins()
+        // grailsHome()
+        // mavenLocal()
+        // grailsCentral()
+        // mavenCentral()
 
-        mavenLocal()
-        mavenCentral()
+        // mavenRepo "http://repository.codehaus.org"
+        // mavenRepo "http://download.java.net/maven/2/"
+        // mavenRepo "http://repository.jboss.com/maven2/"
+	// mavenRepo "http://oss.sonatype.org/content/repositories/releases/"
 
-        // uncomment these (or add new ones) to enable remote dependency resolution from public Maven repositories
-        //mavenRepo "http://snapshots.repository.codehaus.org"
-        //mavenRepo "http://repository.codehaus.org"
-        //mavenRepo "http://download.java.net/maven/2/"
-        //mavenRepo "http://repository.jboss.com/maven2/"
+        mavenRepo "http://mvn.newgenesys.com/content/groups/crs-framework/"
+
     }
 
     dependencies {
         // specify dependencies here under either 'build', 'compile', 'runtime', 'test' or 'provided' scopes e.g.
-
-        // runtime 'mysql:mysql-connector-java:5.1.22'
+        // runtime 'mysql:mysql-connector-java:5.1.24'
     }
 
     plugins {
-        runtime ":hibernate:$grailsVersion"
-        runtime ":jquery:1.10.2"
-        compile ':resources:1.2.RC3'
+        // plugins for the build system only
+        build ':tomcat:7.0.47'
+	runtime ':hibernate:3.6.10.6'
+
+        // plugins for the compile step
+        compile ":scaffolding:2.0.0"
+        compile ':cache:1.1.1'
+
+        // plugins needed at runtime but not for compilation
+        runtime ":database-migration:1.3.8"
+        runtime ":resources:1.2"
 
         // Uncomment these (or add new ones) to enable additional resources capabilities
-        //runtime ":zipped-resources:1.0"
-        //runtime ":cached-resources:1.0"
-        //runtime ":yui-minify-resources:0.1.5"
+        runtime ":zipped-resources:1.0.1"
+        // runtime ":cached-resources:1.1"
+	// compile ":uglify-js-minified-resources:0.1.1"
+        // runtime ":yui-minify-resources:0.1.5"
+	// compile ":yui-war-minify:1.5"
 
-        build ":tomcat:$grailsVersion"
-
-        runtime ":database-migration:1.3.2"
-
-        compile ':cache:1.0.1'
-
-	compile ":platform-core:1.0.RC5"
-	// compile ":html5-mobile-scaffolding:0.6.1"
-	// compile ":jsonp:0.2"
-	// compile ":spring-mobile:0.4"
     }
-
 }
-
-grails.plugin.location.'wep-backend' = '../wep-backend'
-grails.plugin.location.'jquery-mobile' = '../jquery-mobile-grails-plugin'
--- web-app/js/wep-mobile/manager-bootstrap.js
+++
@@ -1,7 +0,0 @@
-var wepmobile = wepmobile || {};
-
-wepmobile.load = (function () {
-    var managerObject = grails.mobile.mvc.manager(wepmobile.configuration);
-
-}());
-
